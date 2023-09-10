0029e258de0c17a55aae3437717dd4291dc5a1d5
Thorsten Reimers <thorsten.reimers@softquadrat.de>
2023-09-10 12:49:23 +0200

    Context menu creation from FileTreeCell.update extracted to own method.

--- src/main/java/de/softquadrat/filemanager/FileTreeCell.java
+++ src/main/java/de/softquadrat/filemanager/FileTreeCell.java
@@ -12,10 +12,12 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.MultipleSelectionModel;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 
 public class FileTreeCell extends TreeCell<File> {
 	private static int counter = 1;
@@ -31,148 +33,159 @@ public class FileTreeCell extends TreeCell<File> {
 			setText(null);
 			setGraphic(null);
 		} else {
-			ContextMenu contextMenu = new ContextMenu();
-			setText(getItem() == null ? "" : getItem().getName());
-			setGraphic(getTreeItem().getGraphic());
-			ObservableList<MenuItem> menuItems = contextMenu.getItems();
-			if (getItem().isDirectory()) {
-				/*
-				 * Refresh Action
-				 */
-				MenuItem refresh = new MenuItem("Refresh");
-				menuItems.add(refresh);
-				// We simply close the folder tree item which will empty the children list.
-				// We will open it afterwards only if it was already open before refresh.
-				refresh.setOnAction(new EventHandler<ActionEvent>() {
-					@Override
-					public void handle(ActionEvent event) {
-						boolean wasExpanded = getTreeItem().isExpanded();
-						getTreeItem().setExpanded(false);
-						if (wasExpanded)
-							getTreeItem().setExpanded(true);
-					}
-				});
-				/*
-				 * Separator
-				 */
-				SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
-				menuItems.add(separatorMenuItem);
-				/*
-				 * Create New File Action
-				 */
-				MenuItem newFile = new MenuItem("New File");
-				menuItems.add(newFile);
-				newFile.setOnAction(new EventHandler<ActionEvent>() {
-					@Override
-					public void handle(ActionEvent event) {
-						TreeItem<File> treeItem = getTreeItem();
-						File dir = treeItem.getValue();
-						if (!dir.isDirectory())
-							return;
-						TextInputDialog dialog = new TextInputDialog("file.txt");
-						dialog.setTitle("New File");
-						dialog.setHeaderText("Creating New File");
-						dialog.setContentText("Please enter name of new file:");
-						Optional<String> result = dialog.showAndWait();
-						if (!result.isPresent())
-							return;
-						File file = new File(dir, result.get());
-						try {
-							if (file.exists()) {
-								Alert error = new Alert(AlertType.WARNING);
-								error.setTitle("Warning");
-								error.setHeaderText("File exists: " + file.getName());
-								error.setContentText("Sorry, ignoring your request because file already exists!");
-								error.showAndWait();
-							} else {
-								file.createNewFile();
-								FileTreeItem newTreeItem = new FileTreeItem(file);
-								treeItem.getChildren().add(newTreeItem);
-							}
-							treeItem.setExpanded(true);
-						} catch (IOException e) {
-							Alert error = new Alert(AlertType.ERROR);
-							error.setTitle("Error");
-							error.setHeaderText("Error creating File: " + file.getName());
-							error.setContentText("Sorry, failed to create file: " + e.getMessage());
-							error.showAndWait();
-						}
-					}
-				});
-				/*
-				 * Create New Directory Action
-				 */
-				MenuItem newDir = new MenuItem("New Directory");
-				menuItems.add(newDir);
-				newDir.setOnAction(new EventHandler<ActionEvent>() {
-					@Override
-					public void handle(ActionEvent event) {
-						TreeItem<File> treeItem = getTreeItem();
-						File dir = treeItem.getValue();
-						if (!dir.isDirectory())
-							return;
-						TextInputDialog dialog = new TextInputDialog("directory");
-						dialog.setTitle("New Directory");
-						dialog.setHeaderText("Creating New Directory");
-						dialog.setContentText("Please enter name of new directory:");
-						Optional<String> result = dialog.showAndWait();
-						if (!result.isPresent())
-							return;
-						File file = new File(dir, result.get());
-						if (file.exists()) {
-							Alert error = new Alert(AlertType.WARNING);
-							error.setTitle("Warning");
-							error.setHeaderText("Directory exists: " + file.getName());
-							error.setContentText("Sorry, ignoring your request because directory already exists!");
-							error.showAndWait();
-						} else {
-							boolean success = file.mkdir();
-							if (!success) {
-								Alert error = new Alert(AlertType.ERROR);
-								error.setTitle("Error");
-								error.setHeaderText("Error creating Directory: " + file.getName());
-								error.setContentText("Sorry, failed to create directory!");
-								error.showAndWait();
-							} else {
-								FileTreeItem newTreeItem = new FileTreeItem(file);
-								treeItem.getChildren().add(newTreeItem);
-							}
-						}
-						treeItem.setExpanded(true);
-					}
-				});
+			File item = getItem();
+			if (item == null) {
+				setText("");
+			} else {
+				setText(item.getName());
+				setContextMenu(createContextMenu(item));
 			}
+			setGraphic(getTreeItem().getGraphic());
+		}
+	}
+
+	private ContextMenu createContextMenu(File item) {
+		ContextMenu contextMenu = new ContextMenu();
+		ObservableList<MenuItem> menuItems = contextMenu.getItems();
+		if (item.isDirectory()) {
 			/*
-			 * Delete File Action
+			 * Refresh Action
 			 */
-			MenuItem deleteFile = new MenuItem("Delete");
-			menuItems.add(deleteFile);
-			deleteFile.setOnAction(new EventHandler<ActionEvent>() {
+			MenuItem refresh = new MenuItem("Refresh");
+			menuItems.add(refresh);
+			// We simply close and reopen the folder tree item which will empty the children list.
+			// But only if it was already open before refresh.
+			refresh.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent event) {
 					TreeItem<File> treeItem = getTreeItem();
-					File file = treeItem.getValue();
-					Alert alert = new Alert(AlertType.CONFIRMATION);
-					alert.setTitle("Delete File");
-					alert.setHeaderText("Delete File: " + file.getName());
-					alert.setContentText("Are you sure?");
-					Optional<ButtonType> result = alert.showAndWait();
-					if (result.get() == ButtonType.OK) {
-						boolean delete = file.delete();
-						if (!delete) {
-							Alert error = new Alert(AlertType.ERROR);
-							error.setTitle("Error");
-							error.setHeaderText("Error deleting File: " + file.getName());
-							error.setContentText("Sorry, deletion failed! Maybe not empty directory?");
-							error.showAndWait();
-						} else {
-							TreeItem<File> parent = treeItem.getParent();
-							parent.getChildren().remove(treeItem);
-						}
+					// Avoid NullPointerException!
+					if (treeItem != null && treeItem.isExpanded()) {
+						treeItem.setExpanded(false);
+						treeItem.setExpanded(true);
 					}
 				}
 			});
-			setContextMenu(contextMenu);
+			/*
+			 * Separator
+			 */
+			SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+			menuItems.add(separatorMenuItem);
+			/*
+			 * Create New File Action
+			 */
+			MenuItem newFile = new MenuItem("New File");
+			menuItems.add(newFile);
+			newFile.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					TreeItem<File> treeItem = getTreeItem();
+					File dir = treeItem.getValue();
+					if (!dir.isDirectory())
+						return;
+					TextInputDialog dialog = new TextInputDialog("file.txt");
+					dialog.setTitle("New File");
+					dialog.setHeaderText("Creating New File");
+					dialog.setContentText("Please enter name of new file:");
+					Optional<String> result = dialog.showAndWait();
+					if (!result.isPresent())
+						return;
+					File file = new File(dir, result.get());
+					try {
+						if (file.exists()) {
+							Alert error = new Alert(AlertType.WARNING);
+							error.setTitle("Warning");
+							error.setHeaderText("File exists: " + file.getName());
+							error.setContentText("Sorry, ignoring your request because file already exists!");
+							error.showAndWait();
+						} else {
+							file.createNewFile();
+							FileTreeItem newTreeItem = new FileTreeItem(file);
+							treeItem.getChildren().add(newTreeItem);
+						}
+						treeItem.setExpanded(true);
+					} catch (IOException e) {
+						Alert error = new Alert(AlertType.ERROR);
+						error.setTitle("Error");
+						error.setHeaderText("Error creating File: " + file.getName());
+						error.setContentText("Sorry, failed to create file: " + e.getMessage());
+						error.showAndWait();
+					}
+				}
+			});
+			/*
+			 * Create New Directory Action
+			 */
+			MenuItem newDir = new MenuItem("New Directory");
+			menuItems.add(newDir);
+			newDir.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					TreeItem<File> treeItem = getTreeItem();
+					File dir = treeItem.getValue();
+					if (!dir.isDirectory())
+						return;
+					TextInputDialog dialog = new TextInputDialog("directory");
+					dialog.setTitle("New Directory");
+					dialog.setHeaderText("Creating New Directory");
+					dialog.setContentText("Please enter name of new directory:");
+					Optional<String> result = dialog.showAndWait();
+					if (!result.isPresent())
+						return;
+					File file = new File(dir, result.get());
+					if (file.exists()) {
+						Alert error = new Alert(AlertType.WARNING);
+						error.setTitle("Warning");
+						error.setHeaderText("Directory exists: " + file.getName());
+						error.setContentText("Sorry, ignoring your request because directory already exists!");
+						error.showAndWait();
+					} else {
+						boolean success = file.mkdir();
+						if (!success) {
+							Alert error = new Alert(AlertType.ERROR);
+							error.setTitle("Error");
+							error.setHeaderText("Error creating Directory: " + file.getName());
+							error.setContentText("Sorry, failed to create directory!");
+							error.showAndWait();
+						} else {
+							FileTreeItem newTreeItem = new FileTreeItem(file);
+							treeItem.getChildren().add(newTreeItem);
+						}
+					}
+					treeItem.setExpanded(true);
+				}
+			});
 		}
+		/*
+		 * Delete File Action
+		 */
+		MenuItem deleteFile = new MenuItem("Delete");
+		menuItems.add(deleteFile);
+		deleteFile.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				TreeItem<File> treeItem = getTreeItem();
+				File file = treeItem.getValue();
+				Alert alert = new Alert(AlertType.CONFIRMATION);
+				alert.setTitle("Delete File");
+				alert.setHeaderText("Delete File: " + file.getName());
+				alert.setContentText("Are you sure?");
+				Optional<ButtonType> result = alert.showAndWait();
+				if (result.get() == ButtonType.OK) {
+					boolean delete = file.delete();
+					if (!delete) {
+						Alert error = new Alert(AlertType.ERROR);
+						error.setTitle("Error");
+						error.setHeaderText("Error deleting File: " + file.getName());
+						error.setContentText("Sorry, deletion failed! Maybe non-empty directory?");
+						error.showAndWait();
+					} else {
+						TreeItem<File> parent = treeItem.getParent();
+						parent.getChildren().remove(treeItem);
+					}
+				}
+			}
+		});
+		return contextMenu;
 	}
 }
